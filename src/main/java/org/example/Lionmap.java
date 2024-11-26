9b61563dbc159c89c0b589e4c431027d86739dd6
linxili <linxili2004@gmail.com>
2024-11-26 11:31:08 +0000

    Initial commit

---
+++ src/main/java/org/example/Lionmap.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.List;
+
+public class Map {
+    public void displayLocations (List<Lion> lions,List<Keeper> keepers,List<Drone>drones) {
+        JFrame frame = new JFrame();
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        JPanel Lionpanel = new JPanel();
+        Lionpanel.setLayout(null);
+
+
+
+
+    }
+}
